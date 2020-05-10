e10690c233bcea425e5bdd51b7b2ac7814124d33
kwiaciu <bkwiatek3@gmail.com>
2020-05-10 14:28:25 +0200

    interval added

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -10,40 +10,40 @@ export class GalleryComponent {
     {
       src:
         'https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1348&q=80',
-      alt: 'opis 1',
+      alt: 'basket of cats',
     },
     {
       src:
         'https://images.unsplash.com/photo-1529257414772-1960b7bea4eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-      alt: 'opis 2',
+      alt: 'cat',
     },
     {
       src:
         'https://images.unsplash.com/photo-1532386236358-a33d8a9434e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1278&q=80',
-      alt: 'opis 3',
+      alt: 'two cats',
     },
   ];
-  currentIndex = 0;
+  currentIndex: number = 0;
 
-  handleClick(index) {
+  handleClick(index: number): void {
     this.currentIndex = index;
   }
 
-  handlePrev(index) {
-    this.currentIndex = index - 1;
+  handlePrev(): void {
+    this.currentIndex = this.currentIndex - 1;
   }
 
-  handleNext(index) {
-    this.currentIndex = index + 1;
+  handleNext(): void {
+    this.currentIndex = this.currentIndex + 1;
   }
 
   constructor() {
-    // setInterval(() => {
-    //   if (this.currentIndex < this.pictures.length - 1) {
-    //     this.currentIndex++;
-    //   } else {
-    //     this.currentIndex = 0;
-    //   }
-    // }, 5000);
+    setInterval(() => {
+      if (this.currentIndex < this.pictures.length - 1) {
+        this.currentIndex++;
+      } else {
+        this.currentIndex = 0;
+      }
+    }, 5000);
   }
 }
